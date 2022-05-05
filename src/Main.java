37899029445468c2ba0ec3f719f2bbd972b48a75
pexorka <pexorka@ya.ru>
2022-05-05 09:54:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int miles = ticketPrice / 20;
+        System.out.println("Количество ваших бонусных миль: " + (miles));
+    }
+}
